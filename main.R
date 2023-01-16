5f4c4b266bef594793720ad3e4cf123a169e4a7b
Braz___ <bcalderonprieto@gmail.com>
2023-01-15 22:34:59 -0500

    Codigo mas organizado y guardado de imagenes para el informe

--- main.R
+++ main.R
@@ -1,41 +1,49 @@
-
-install.packages("readxl")
-library("readxl")
-my_data <- read_excel("paises.xls",sheet=1)
 install.packages("easypackages")
+lib_req <- c("devtools", "readxl", "lubridate", "dplyr", "visdat", "missMDA", "mice", "DMwR2", "editrules", "corrplot") # Librerías requeridas
+easypackages::packages(lib_req) # Instalación de librerías requeridas
 
-install.packages("devtools")
-lib_req<-c("lubridate","dplyr","visdat","missMDA","mice","DMwR2","editrules", "corrplot") #librerias que se requieren
-easypackages::packages(lib_req) #se verifican las librerias instaladas
+
+# Lectura de archivo Excel---
+library("readxl")
+my_data <- read_excel("paises.xls", sheet = 1)
+
+# --------------------------- Análisis pre-limpieza -------------------------- #
+
+
+is.na(my_data) # For each element in "my_data" verifies if is NA
+jpeg(filename = "images/1.jpeg", width = 800, height = 600) # Iniciar proceso de guardado
+#x11()
+visdat::vis_miss(my_data) # Función para visualizar datos faltantes
+dev.off() # Terminar proceso de guardado
 
 #-------------------------------------------------------------------------------
 # ----------------------------NIVELES PARA FACTORES-----------------------------
 #-------------------------------------------------------------------------------
-install.packages("dplyr")
+
 library(dplyr)
 table(my_data$GRUPOS)
-#Declaro los niveles correctos
-level_group <- c(africa="africa",Africa="africa",AFRICA="africa",asia="asia"
-                 ,Asia="asia",ASIA="asia",`Europa Oriental`="europa oriental"
-                 ,`EUROPA ORIENTAL`="europa oriental",`ORIENTE MEDIO`="oriente medio"
-                 ,iberoamerica="iberoamerica",Iberoamerica="iberoamerica",IBEROAMERICA="iberoamerica",
-                 `EO-NA_JAPON_AUSTR_NZ`="eo-na_japon_austr_nz"
-                 )
 
-#Actualizo my_data
+# Declaro los niveles correctos
+level_group <- c(
+                africa = "Africa", Africa = "Africa", AFRICA = "Africa",
+                asia = "Asia", Asia = "Asia", ASIA = "Asia",
+                `Europa Oriental` = "Europa Oriental", `EUROPA ORIENTAL` = "Europa Oriental",
+                `ORIENTE MEDIO` = "Oriente Medio",
+                iberoamerica = "Iberoamerica", Iberoamerica = "Iberoamerica", IBEROAMERICA = "Iberoamerica",
+                `EO-NA_JAPON_AUSTR_NZ` = "eo-na_japon_austr_nz"
+                )
 
-my_data <- transform(my_data,GRUPOS=factor(dplyr::recode(GRUPOS,!!!level_group)))
+# Actualizo my_data
+my_data <- transform(my_data, GRUPOS = factor(dplyr::recode(GRUPOS, !!!level_group)))
 str(my_data)
 
-#Reglas
-install.packages("editrules")
+# Cargo el archivo de las reglas
 library(editrules)
+rules <- editrules::editfile("consistencia.txt")
+rules
 
-#Cargo el archivo de las reglas
-Rules <-editrules::editfile("rules.txt")
-Rules
 #Ver como se relacionan las reglas
 #x11()
 #plot(Rules)
-valid_data=editrules::violatedEdits(Rules, my_data)
-summary(valid_data) #Puesto que sale Null ningun valor viola las reglas
+valid_data = editrules::violatedEdits(rules, my_data)
+summary(valid_data) # Puesto que sale Null ningún valor viola las reglas
